c2b1d898fc49f4f4e4f0138e809db11d5c8c64df
Thomas Pearson <thomaspearson.dev@gmail.com>
2023-08-05 13:06:43 +1200

    chore: fix up linting config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard-with-typescript', 'eslint:recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+  },
+};
