2934189c5abf6c3e5204fbb67f9626c044842534
Thomas Pearson <thomaspearson.dev@gmail.com>
2023-08-11 08:41:44 +1200

    refactor(server): health check add common response

--- server/src/routes/probes/healthz.ts
+++ server/src/routes/probes/healthz.ts
@@ -4,21 +4,29 @@ import { withAsyncErrorHandling } from '../middleware/withAsyncErrorHandling';
 import { ordersRepo } from '../../repos/ordersRepo';
 import config from '../../config';
 
+const response = {
+  buildNumber: config.BUILD_NUMBER,
+  commitHash: config.COMMIT_HASH,
+};
+
 export const handleGetHealthz: RequestHandler = withAsyncErrorHandling(
   async (req, res) => {
     try {
       // const queryResult = await pool.query(`SELECT 1`);
-      const queryResult = await ordersRepo.getOrders();
-      if (queryResult) {
-        res.status(200).json({
-          buildNumber: config.BUILD_NUMBER,
-          commitHash: config.COMMIT_HASH,
-        });
-        return;
+      const connection = await ordersRepo.connection();
+
+      switch (connection) {
+        case 'CONNECTION_OK':
+          return res.status(200).json(response);
+        case 'CONNECTION_BAD':
+          logger.error(
+            'Health check failed, due to repository connection error'
+          );
+          return res.status(500).json(response);
       }
     } catch (e) {
       logger.error('Health check failed', e);
+      res.sendStatus(500).json(response);
     }
-    res.sendStatus(500);
   }
 );
